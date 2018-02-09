ede596bb2eb27c8a128fb40ef1ef0a27cea13b15
MANUEL GIL <manuel_gilb@hotmail.com>
2018-02-09 01:40:25 -0400

    Se implemenetó el código para leer el archivo init.txt

---
+++ mips1.asm
@@ -0,0 +1,45 @@
+.data
+	archivo: .asciiz "/home/manuel/Documentos/Orga/init.txt" # Nombre del archivo a inicializar
+	buffer: .space 1024
+	space: .asciiz "\n"
+
+.text
+
+main:
+
+	# Abrir el archivo para escribir
+	li   $v0, 13       # Instrucción a syscall para abrir un archivo
+	la   $a0, archivo      # Nombre del archivo txt el cual deseamos abrir
+	li   $a1, 0        # Abrir para leer
+	li   $a2, 0
+	syscall            # Abre el archivo y retorna la descripción en el  registro $v0
+	move $s6, $v0      # guardamos la descripción del archivo en $s6
+
+	move $a0, $v0
+	
+	li $v0, 1
+	syscall
+	
+	addi $v0, $zero, 4  # print_string syscall
+    	la $a0, space       # load address of the string
+    	syscall
+
+	#read from file
+	li   $v0, 14       # system call for read from file
+	move $a0, $s6      # file descriptor 
+	la   $a1, buffer   # address of buffer to which to read
+	li   $a2, 1024     # hardcoded buffer length
+	syscall            # read from file
+	
+	move $a0, $a1
+	
+	li $v0, 4
+	syscall
+	
+	# Close the file 
+	li   $v0, 16       # system call for close file
+	move $a0, $s6      # file descriptor to close
+	syscall            # close file
+	
+	li $v0, 10 #fin del programa
+	syscall
